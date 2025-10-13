deed10ee163244842b59cbc02e646e8e59d1c3da
DevCode102 <borix.dex@gmail.com>
2025-10-13 19:58:07 +0100

    Fix 32

--- src/app/api/packages/route.ts
+++ src/app/api/packages/route.ts
@@ -6,9 +6,9 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function GET(req: Request) {
-    const session = await getServerSession(authOptions);
-    if (!session)
-        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    // const session = await getServerSession(authOptions);
+    // if (!session)
+    //     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
 
     try {
         const { searchParams } = new URL(req.url);
