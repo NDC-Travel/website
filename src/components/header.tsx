305054bf1039b36f0ab4235137f84283cd769cb4
DevCode102 <borix.dex@gmail.com>
2025-10-14 18:44:43 +0100

    Fix 37

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -259,6 +259,7 @@ export default function Header() {
                                         <AvatarFallback className={'!text-decoration-none !bg-black !text-white'}>{session.user?.name?.charAt(0)}</AvatarFallback>
                                     </Avatar>
                                 </Link>
+
                                 <Sheet>
                                     <SheetTrigger asChild>
                                         <Button variant="outline" size="icon" className="!h-[40px] !m-0 !rounded-full !w-[40px]">
@@ -305,51 +306,54 @@ export default function Header() {
                                 </Sheet>
                             </div>
                             :
-                            <Drawer open={openAuth} onOpenChange={setOpen}>
-                                <DrawerTrigger onClick={() => setOpen(true)}>
-                                    <User2Icon className={'w-8 h-8 me-5'} />
-                                </DrawerTrigger>
-                                <DrawerContent>
-                                    <DrawerHeader>
-                                        <DrawerTitle>
-                                            <Link href="/" className="navbar-brand py-1 py-md-2 py-xl-1 me-2 me-sm-n4 me-md-n5 me-lg-0 flex items-center mb-10">
-                                                <Image alt={"NDC Travels"} width={100} height={100} src={"/appIcon.png"} className={'w-[50px] h-auto'} />
-                                                <Image alt={"NDC Travels"} width={100} height={100} src={"/ndc.png"} className={'w-[70px] h-auto'} />
-                                            </Link>
-                                        </DrawerTitle>
-                                        <DrawerDescription>
-                                            <span className="h2 mt-auto">Bienvenue sur NDC Travels</span><br/>
-                                            <div className="d-flex align-items-center my-4">
-                                                <hr className="w-100 m-0"/>
-                                                <span
-                                                    className="text-body-emphasis fw-medium text-nowrap mx-4">
-                                            <small className={'text-[1.15rem] text-muted'}>Connectez-vous </small>
-                                        </span>
-                                                <hr className="w-100 m-0"/>
-                                            </div>
-
-                                            <div className={'!flex !items-center !justify-center !w-full'}>
-                                                <div className={'!max-w-[700px] !w-[450px]'}>
-                                                    <Login />
-                                                </div>
-                                            </div>
-                                        </DrawerDescription>
-                                    </DrawerHeader>
-
-                                    <DrawerFooter className={'flex w-full justify-center'}>
-                                        <p className="text-body-secondary fs-sm text-center mb-0 order-sm-1">
-                                            © Copyright <a
-                                            className="text-primary fw-bold text-decoration-none hover-effect-underline"
-                                            href="/"
-                                            target="_blank"
-                                            rel="noreferrer"
-                                        >
-                                            NDC Travel
-                                        </a>{' '}2025. Tous les droits sont reservés. {' '}
-                                        </p>
-                                    </DrawerFooter>
-                                </DrawerContent>
-                            </Drawer>
+                            <Link href={"/auth/signin"} className={'!text-decoration-none'}>
+                                <User2Icon className={'w-8 h-8 me-5'} />
+                            </Link>
+                            // <Drawer open={openAuth} onOpenChange={setOpen}>
+                            //     <DrawerTrigger onClick={() => setOpen(true)}>
+                            //         <User2Icon className={'w-8 h-8 me-5'} />
+                            //     </DrawerTrigger>
+                            //     <DrawerContent>
+                            //         <DrawerHeader>
+                            //             <DrawerTitle>
+                            //                 <Link href="/" className="navbar-brand py-1 py-md-2 py-xl-1 me-2 me-sm-n4 me-md-n5 me-lg-0 flex items-center mb-10">
+                            //                     <Image alt={"NDC Travels"} width={100} height={100} src={"/appIcon.png"} className={'w-[50px] h-auto'} />
+                            //                     <Image alt={"NDC Travels"} width={100} height={100} src={"/ndc.png"} className={'w-[70px] h-auto'} />
+                            //                 </Link>
+                            //             </DrawerTitle>
+                            //             <DrawerDescription>
+                            //                 <span className="h2 mt-auto">Bienvenue sur NDC Travels</span><br/>
+                            //                 <div className="d-flex align-items-center my-4">
+                            //                     <hr className="w-100 m-0"/>
+                            //                     <span
+                            //                         className="text-body-emphasis fw-medium text-nowrap mx-4">
+                            //                 <small className={'text-[1.15rem] text-muted'}>Connectez-vous </small>
+                            //             </span>
+                            //                     <hr className="w-100 m-0"/>
+                            //                 </div>
+                            //
+                            //                 <div className={'!flex !items-center !justify-center !w-full'}>
+                            //                     <div className={'!max-w-[700px] !w-[450px]'}>
+                            //                         <Login />
+                            //                     </div>
+                            //                 </div>
+                            //             </DrawerDescription>
+                            //         </DrawerHeader>
+                            //
+                            //         <DrawerFooter className={'flex w-full justify-center'}>
+                            //             <p className="text-body-secondary fs-sm text-center mb-0 order-sm-1">
+                            //                 © Copyright <a
+                            //                 className="text-primary fw-bold text-decoration-none hover-effect-underline"
+                            //                 href="/"
+                            //                 target="_blank"
+                            //                 rel="noreferrer"
+                            //             >
+                            //                 NDC Travel
+                            //             </a>{' '}2025. Tous les droits sont reservés. {' '}
+                            //             </p>
+                            //         </DrawerFooter>
+                            //     </DrawerContent>
+                            // </Drawer>
                     }
 
                     <Link href="/carry" className="btn !hidden lg:!flex btn-primary animate-shake me-2 h-[40px]">
