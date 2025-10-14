f8cad844677d2603a9350fe512a7a164b615a6cc
DevCode102 <borix.dex@gmail.com>
2025-10-14 16:39:04 +0100

    Fix 37

--- src/app/package/page.tsx
+++ src/app/package/page.tsx
@@ -4,12 +4,25 @@ import Breadcrumb from "@/components/nav";
 import ListingFilter from "@/components/filter";
 import {Separator} from "@/components/ui/separator";
 import ListingPackage from "@/components/list";
+import {useSession} from "next-auth/react";
+import {useRouter} from "next/navigation";
+import {use, useEffect, useState} from "react";
 
 export default function Listing({
                                     searchParams,
                                 }: {
     searchParams: Promise<{ origin?: string, search?: string, destination?: string }>
 }) {
+
+    const { data: session, status } = useSession();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (status === "unauthenticated") {
+            router.push("/auth/signin");
+        }
+    }, [status, router]);
+
     return (
         <main className={'content-wrapper'}>
 
